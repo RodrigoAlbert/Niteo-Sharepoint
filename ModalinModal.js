80fa0d517a044ed03fdc794b2784cb2935ceb23d
RodrigoAlbert <rodrigo.albert@outlook.com>
2016-11-03 11:19:56 -0200

    Update ModalinModal.js

--- ModalinModal.js
+++ ModalinModal.js
@@ -1,1 +1,5 @@
-$('.ms-dlgContent').prevObject[0].activeElement.href = page + "?IsDlg=1"; 
+$('.ms-dlgContent').prevObject[0].activeElement.href = page + "?IsDlg=1"; // Possível problema de click no IE.
+ 
+
+
+location.replace(url + "?IsDlg=1"); // Menor e mais eficiente nos navegadores.
